cc4ba9d86c7211806002a47c237d2a7e18b0e0c2
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2026-02-21 12:15:09 +0800

    chore: replace @radix-ui/react components with radix-ui package; update dependencies in package.json and bun.lock

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { IconCheck } from '@tabler/icons-react'
+import { Checkbox as CheckboxPrimitive } from 'radix-ui'
 import type * as React from 'react'
 
 import { cn } from '@/lib/cn'
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import * as LabelPrimitive from '@radix-ui/react-label'
 import { IconAsterisk } from '@tabler/icons-react'
+import { Label as LabelPrimitive } from 'radix-ui'
 
 import { cn } from '@/lib/cn'
 
